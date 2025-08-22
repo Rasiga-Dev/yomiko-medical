0ca36f9ba09d8bab78b523ca4f436f14d3159b7e
Jeromesaleth <jeromesaleth2000@gmail.com>
2025-08-22 11:56:35 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yamiko-medical/', // Replace 'repo-name' with your actual repository name
+  base: '/yamiko_medical/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
