aacf81973ba73c9eec58cf338868132fd495999d
Rasiga-Dev <rasiga04112002@gmail.com>
2025-11-18 11:22:42 +0530

    Updated website

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// change this to your repo name
+const repoName = 'yomiko-medical'
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
-  base: '/yamiko-medical/', // IMPORTANT for GitHub Pages under repo
 })
