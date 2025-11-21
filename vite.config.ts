bc3ad1210db6b973439c8906e62baf7fb6c63d00
Rasiga-Dev <rasiga04112002@gmail.com>
2025-11-21 17:26:26 +0530

    Added GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/yomiko-medical/',  // <– உங்கள் GitHub repo name
+  base: '/yomiko-medical/',  // GitHub repo name
 })
